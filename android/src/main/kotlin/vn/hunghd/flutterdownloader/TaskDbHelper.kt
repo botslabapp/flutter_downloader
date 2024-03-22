61562731e160ac59ccd09589155ee9e5656bdbe9
lifeifei1 <lifeifei1@360.cn>
2024-03-22 16:12:32 +0800

    feat:Task数据库版本更新时删除表重新创建

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/TaskDbHelper.kt
@@ -12,14 +12,16 @@ class TaskDbHelper private constructor(context: Context) :
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        if (newVersion == 4) {
-            db.execSQL("ALTER TABLE ${TaskEntry.TABLE_NAME} ADD COLUMN ${TaskEntry.COLUMN_ALLOW_CELLULAR} TINYINT DEFAULT 1")
-        } else if (oldVersion == 2 && newVersion == 3) {
-            db.execSQL("ALTER TABLE " + TaskEntry.TABLE_NAME + " ADD COLUMN " + TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0")
-        } else {
-            db.execSQL(SQL_DELETE_ENTRIES)
-            onCreate(db)
-        }
+//        if (newVersion == 4) {
+//            db.execSQL("ALTER TABLE ${TaskEntry.TABLE_NAME} ADD COLUMN ${TaskEntry.COLUMN_ALLOW_CELLULAR} TINYINT DEFAULT 1")
+//        } else if (oldVersion == 2 && newVersion == 3) {
+//            db.execSQL("ALTER TABLE " + TaskEntry.TABLE_NAME + " ADD COLUMN " + TaskEntry.COLUMN_SAVE_IN_PUBLIC_STORAGE + " TINYINT DEFAULT 0")
+//        } else {
+//            db.execSQL(SQL_DELETE_ENTRIES)
+//            onCreate(db)
+//        }
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
     }
 
     override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
